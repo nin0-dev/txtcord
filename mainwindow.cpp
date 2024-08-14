7f027c58d2800d0a79da1ef34cdc803bd48369df
nin0dev <personal@nin0.dev>
2024-08-14 15:23:01 -0400

    things :potOfGreed:

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,18 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 
+void MainWindow::addMessage(QString content, MainWindow::MessageType type) {
+    if(type == Normal) {
+        m_chatlog->append(content);
+    }
+    if(type == System) {
+        m_chatlog->append(QString("<span style=\"color:#00FFE5;\">[Sys] %1</p>").arg(content));
+    }
+    if(type == Error) {
+        m_chatlog->append(QString("<span style=\"color:#ff0000;\">Error: %1</p>").arg(content));
+    }
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
@@ -13,7 +25,8 @@ MainWindow::MainWindow(QWidget *parent)
     // init chatlog
     m_chatlog = new QTextEdit();
     m_chatlog->setReadOnly(true);
-    m_chatlog->append("<b style=\"color:#00FFE5;\">Welcome to txtcord, the minimal Discord client.");
+    m_chatlog->append("<b style=\"color:#00FFE5;\">Welcome to txtcord, the minimal Discord client.</b>");
+    addMessage("No accounts are saved. Use the /login [email/phone] command to sign in to your Discord account.", System);
     m_layout->addWidget(m_chatlog);
     // init chatbox
     QWidget *chatboxWidget = new QWidget();
@@ -26,8 +39,6 @@ MainWindow::MainWindow(QWidget *parent)
     chatboxLayout->addWidget(m_send);
     chatboxWidget->setLayout(chatboxLayout);
     m_layout->addWidget(chatboxWidget);
-    // init chatlog
-
 }
 
 MainWindow::~MainWindow() {
--- mainwindow.h
+++ mainwindow.h
@@ -14,9 +14,11 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    enum MessageType {Normal, System, Error};
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private:
+    void addMessage(QString content, MainWindow::MessageType type = Normal);
     QVBoxLayout *m_layout;
     QTextEdit *m_chatlog;
     QLineEdit *m_box;
